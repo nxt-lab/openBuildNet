5caf3266b8aa09ce6c36b810357a8d728a09ec5c
Truong Nghiem <truong.nghiem@gmail.com>
2016-01-12 17:46:06 +0100

    Finished tests/testadmm to demo how to use OBN and Matlab nodes to implement ADMM distributed optimization.

---
+++ tests/testadmm/admm_tracking/Aggregator.m
@@ -0,0 +1,242 @@
+% Define a class that implements the global node of distributed optimization.
+% Run this node with runglobal.m.
+%
+% This file is part of the openBuildNet simulation framework developed at
+% EPFL.
+%
+% Authors: Truong X. Nghiem (xuan.nghiem@epfl.ch)
+
+classdef Aggregator < OBNNode
+    properties (Constant)
+        % Define the indices of the updates
+        MAIN_UPDATE = 0;    % the main update that performs the ADMM iterations
+        
+        % the workspace is the same for all nodes
+        WORKSPACE = 'disttracking';
+        
+        COMM_TYPE = 'mqtt';             % the communication type, yarp or mqtt
+        COMM_PARAMS = '';               % the communication parameter
+
+        % Any constant parameters can be placed here
+    end
+    
+    % Internal variables of this particular node, e.g. state variables
+    properties (Access=private)
+        Nnode       % The number of local nodes
+        Nhorizon    % length of horizon
+        Gammas      % quadratic cost parameter to be sent to locals
+        deltas      % affine cost parameter to be sent to locals
+        Z           % local references stacked in a matrix
+        Z_prev      % Z at previous step, used for residual calculation
+        MU          % local duals stacked in a matrix
+        lambda      % global dual
+        rho         % admm stepsize
+        T           % summation matrix for references r = Tz
+        err         % tracking error
+        refHorizon  % reference for the horizon
+        reference   % reference trajectory for simulation
+        step        % step
+        delta_t     % sampling time MIGHT NOT BE NECESSARY
+        U           % proposed inputs from locals stacked in a matrix
+        epsPrimal = 1e-5; % primal convergence criterion
+        epsDual = 1e-5 ;  % dual convergence criterion
+        resDual    % residual for dual convergence 
+        resPrimal   % residual for primal convergence
+        maxIter = 50;
+        iter = 0;
+        
+        admm_events                     % To track receipts from U ports
+        in_admm_iters = false;          % true if and only if ADMM iterations are ongoing
+    end
+    
+    methods
+        function obj = Aggregator(Nnode, rho, Nhorizon, Ref)
+            % N = the number of local nodes, numberred from 1 to N
+            assert(isnumeric(Nnode) && isscalar(Nnode) && Nnode >= 1);
+            assert(isnumeric(Nhorizon) && isscalar(Nhorizon) && Nhorizon >= 1);
+            assert(isnumeric(Ref) && length(Ref) >= Nhorizon)
+            
+            obj = obj@OBNNode('Aggregator', Aggregator.WORKSPACE, Aggregator.COMM_TYPE, ...
+                              Aggregator.COMM_PARAMS);
+            obj.Nnode = Nnode;
+            obj.Nhorizon = Nhorizon;
+            obj.reference = Ref;
+            obj.rho = rho;
+            obj.lambda = zeros(Nhorizon,1);
+            obj.err = zeros(Nhorizon,1);
+            obj.T =  kron(ones(1,Nnode),eye(Nhorizon));
+            
+            obj.Gammas = cell(1,Nnode);
+            obj.deltas = cell(1,Nnode);
+            obj.U = zeros(Nhorizon,Nnode);
+            obj.Z = zeros(Nhorizon,Nnode);
+            obj.Z_prev = zeros(Nhorizon,Nnode);
+            obj.MU = zeros(Nhorizon,Nnode);
+
+            obj.step = 1;
+            
+            obj.admm_events = false(Nnode,1);
+            
+            % Create individual input(s) for each local node to receive
+            % values from that node, for example x1 to xN as vectors.
+            for k = 1:Nnode
+                UportName = ['U' int2str(k)];
+                obj.createInputPort(UportName, 'v', 'double');
+                obj.createOutputPort(['Gamma'  int2str(k)], 'm', 'double');
+                obj.createOutputPort(['delta' int2str(k)], 'v', 'double');
+                
+                % Add RCV callback on the U port
+                obj.addCallback(@obj.admmiter, 'RCV', UportName, k);
+            end
+            
+            % Register callbacks
+            obj.addCallback(@obj.main_update, 'Y', obj.MAIN_UPDATE);
+
+            obj.addCallback(@obj.onInit, 'INIT');
+            obj.addCallback(@obj.simple_callback, 'TERM', 'Simulation is terminated');
+        end
+        
+    end
+    
+    methods (Access=private)
+        % These are the callbacks
+        function simple_callback(this, s)
+            fprintf('At %d %s\n', this.currentSimTime, s);
+        end
+        
+        function onInit(this)
+            % This function is called everytime the simulation starts, so
+            % use it to initialize the state and outputs of this node.
+            % Outputs to local nodes can be set here, however not really
+            % necessary because they will be calculated by iter_update() at
+            % t = 0.
+            
+            fprintf('At %d INIT\n', this.currentSimTime);
+        end
+        
+        function iter_computation(this)
+            % Perform the iteration computation here
+            % local dual update
+            this.MU = this.MU + this.U - this.Z;
+            % local reference update
+            G = (eye(this.Nhorizon*this.Nnode) + this.T'*this.T);
+            f = vec(this.U)+vec(this.MU) + this.T'*(this.refHorizon - this.lambda);
+            z = G\f;
+            this.Z_prev = this.Z;
+            this.Z = vec2mat(z,this.Nhorizon)';
+            this.err = this.T*z-this.refHorizon ;
+            % global dual update
+            this.lambda = this.lambda + this.err;
+            
+            % Write to the output port(s) to send values to the local
+            % nodes immediately.
+            for k = 1:this.Nnode
+                this.Gammas{k} = this.rho*eye(this.Nhorizon);
+                this.deltas{k} = this.rho*(-this.Z(:,k) + this.MU(:,k));
+                this.sendSync(['Gamma' int2str(k)],this.Gammas{k});
+                this.sendSync(['delta' int2str(k)],this.deltas{k});
+            end
+        end
+
+        
+        function admmiter(this, k)
+        % Callback of the port events, where k is the index of the U<k> port.
+        % It is called every time a local node sends U<i> values to the aggregator.
+        % This callback should perform the ADMM iteration whenever all U<i>'s have been
+        % received.
+        % It should also check the termination condition.
+        % The computation should only be performed if and only if this.in_admm_iters =
+        % true; and when it finishes, it must reset this variable to stop the ADMM iterations.
+            if ~this.in_admm_iters
+                return;
+            end
+            
+            %fprintf('admmiter for k = %d\n', k);
+            
+            % Mark the port event
+            if this.admm_events(k)
+                warning(['During ADMM iteration, values from port U%d have been received ' ...
+                         'multiple times.'], k);
+            end
+            this.admm_events(k) = true;
+            this.U(:,k) = this.input(['U' int2str(k)]); % save the values
+            
+            % If all have been received, perform the iteration computation
+            if all(this.admm_events)
+                this.iter = this.iter + 1;
+                %fprintf('ADMM iteration %d after receiving from all local nodes.\n', this.iter);
+                
+                this.admm_events = false(this.Nnode, 1); % reset tracking                    
+                
+                % the variable continue_iter determines whether to continue
+                % convergence criteria
+                
+                % compute Residuals
+                residualRef = this.T*vec(this.Z)-this.refHorizon;
+                residualLocalTracking = vec(this.U)- vec(this.Z);
+                this.resPrimal = [residualRef;residualLocalTracking];
+                %
+                this.resDual = -this.rho * vec( this.Z - this.Z_prev ) ;
+                
+                % check convergence
+                convPrimal = sqrt(this.resPrimal'*this.resPrimal);
+                convDual = sqrt(this.resDual'*this.resDual);
+                % disp(convPrimal)
+                % disp(convDual)
+                continue_iter = ~((convPrimal <= this.epsPrimal && convDual <= this.epsDual ) || ...
+                                  this.iter >= this.maxIter);
+
+                if continue_iter
+                    this.iter_computation();
+                else
+                    this.iter = 0;
+
+                    % Stop iterations
+                    this.in_admm_iters = false;
+                    
+                    % reset ADMM variables
+                    this.U = zeros(this.Nhorizon,this.Nnode);
+                    this.Z = zeros(this.Nhorizon,this.Nnode);
+                    this.Z_prev = zeros(this.Nhorizon,this.Nnode);
+                    this.MU = zeros(this.Nhorizon,this.Nnode);
+                end
+            end
+        end
+        
+        function main_update(this)
+        % This function is called at every major time step.
+        % It should start the ADMM iterations; process all port events (to perform the ADMM
+        % iterations) until the ADMM algorithm finishes.
+            
+            % the current time, 0 if this is the initial step
+            fprintf('Start ADMM at time %g (s).\n', this.currentSimTime);
+            
+            this.refHorizon = this.reference(this.step:this.step-1+this.Nhorizon) ; % ASK TRUONG
+            this.step = this.step + 1;
+            
+            % Start ADMM
+            this.in_admm_iters = true;
+
+            % Tell local nodes to start iterations
+            this.iter_computation();
+            
+            % Loop to process all port events until it finishes, or a long timeout of 60s.
+            mytimeout = tic;
+            while (this.in_admm_iters && toc(mytimeout) <= 60)
+                if this.processPortEvent(1)
+                    mytimeout = tic;
+                end
+                % We can check this.isRunning()) once in a while to make sure that we don't wait
+                % for a long time when the simulation stops unexpectedly
+            end
+            
+            if this.in_admm_iters
+                % Must be some error, like a timeout
+                this.in_admm_iters = false;
+                this.stopSimulation();
+                disp('ERROR: An error happened during ADMM iterations; likely a timeout.');
+            end
+        end
+                
+    end
+end
---
+++ tests/testadmm/admm_tracking/localController.m
@@ -0,0 +1,242 @@
+% Define a class that implements a local node of distributed optimization.
+% Run this node with runlocal.m.
+%
+% This file is part of the openBuildNet simulation framework developed at
+% EPFL.
+%
+% Authors: Truong X. Nghiem (xuan.nghiem@epfl.ch)
+
+classdef localController < OBNNode
+    properties (Constant)
+        % Define the indices of the updates
+        OBTAINSTATE_UPDATE = 0;    % the update to get the state values from plant
+        CONTROL_UPDATE = 1;             % the update to send the control values to plant
+        
+        % the workspace is the same for all nodes
+        WORKSPACE = 'disttracking';
+        
+        COMM_TYPE = 'mqtt';             % the communication type, yarp or mqtt
+        COMM_PARAMS = '';               % the communication parameter
+        
+        % Any constant parameters (for all local nodes) can be placed here
+    end
+    
+    % Internal variables of this particular node, e.g. state variables
+    properties (Access=private)
+        Nhorizon
+        model
+        xlim
+        ulim
+        mpc
+        u
+        U
+        Gamma
+        delta
+        x0
+        
+        admm_events = [false, false];   % to keep track of receipts of [Gamma, delta]
+    end
+    
+    methods
+        function obj = localController(id,solver, Nhorizon, A,B,xLim,uLim,varargin)
+            % id = the unique ID of this node
+            % Nhorizon =  length of horizon for reference tracking
+            % model = struct with system matrices for the plant
+            % constarints = stagewise constraints for the plant, fields xlim,ulim
+            % constraints.xlim = [xmin, xmax] = state constraints for plant
+            % constraints.ulim = [umin, umax] = input constraints
+            % Q = Quadratic parameter for local stage cost
+            % c = Affine parameter for local stage cost
+            % varargin = cost parameters R,cu,Q,cx,P,cxN
+            
+            
+            % create the node
+            if ischar(id)
+                nodename = ['Controller' id];
+            elseif isnumeric(id)
+                nodename = ['Controller' int2str(id)];
+            else
+                error('Invalid node ID.');
+            end
+            
+            obj = obj@OBNNode(nodename, localController.WORKSPACE, localController.COMM_TYPE, ...
+                              localController.COMM_PARAMS);
+            
+            % Create inputs into this node. The syntax for each input is:
+            %   obj.createInputPort('name_of_port', container, type);
+            % where 'name_of_port' must be a unique valid name; container
+            % is 's' for scalar, 'v' for vector, and 'm' for matrix; type
+            % must be a valid Matlab type name, typically 'double' but can
+            % also be 'logical', 'int64', 'int32', 'uint64', 'uint32'
+            
+            % set PROPERTIES:
+            obj.Nhorizon = Nhorizon;
+            obj.model.A = A;
+            obj.model.B = B;
+            obj.model.xLim = xLim;
+            obj.model.uLim = uLim;
+            %nx = size(A,1);
+            %nu = size(B,2);
+            
+            obj.mpc = obj.MPCsolver(Nhorizon,solver,obj.model,varargin{:});
+
+            % INPUTS:
+            % Quadratic parameter for optimization cost
+            obj.createInputPort('Gamma', 'm', 'double');
+            % Affine parameter for optimization cost
+            obj.createInputPort('delta', 'v', 'double');
+            % current state of the plant
+            obj.createInputPort('x', 's', 'double');
+            
+            % OUTPUTS:
+            % Control input to the plant
+            obj.createOutputPort('u', 's', 'double');
+            % Primary decision variable to be passed to the aggregator
+            % during ADMM iterations
+            obj.createOutputPort('U', 'v', 'double');
+            
+            % Register callbacks
+            obj.addCallback(@obj.obtainstate, 'Y', localController.OBTAINSTATE_UPDATE);
+            obj.addCallback(@obj.computecontrol, 'Y', localController.CONTROL_UPDATE);
+            obj.addCallback(@obj.admmiter, 'RCV', 'Gamma', 1); % 1 means Gamma has been received
+            obj.addCallback(@obj.admmiter, 'RCV', 'delta', 2); % 2 means delta has been received
+                        
+            obj.addCallback(@obj.onInit, 'INIT');
+            obj.addCallback(@obj.simple_callback, 'TERM', 'Simulation is terminated');
+        end
+        
+    end
+    
+    methods (Access=private)
+        
+        % optimizer construction
+        function opt  = MPCsolver(this,N,solver,model,varargin)
+            % creates MPC solver
+            % varargin = cost parameters R,cu,Q,cx,P,cxN
+            
+            nx = size(this.model.A,1);
+            nu = size(this.model.B,2);
+            
+                        
+            % CONSTRAINTS
+            
+            % varargin = R,cu,Q,cx,P,cxN
+            numvarargs = length(varargin);
+            if numvarargs > 6
+                error('too many arguments');
+            end
+            % defualt values
+            optargin = {zeros(nu),zeros(nu,1),...
+                zeros(nx),zeros(nx,1),...
+                zeros(nx),zeros(nx,1)};
+            % replace default values with provided ones
+            optargin(1:numvarargs) = varargin;         
+            
+            [R,cu,...
+             Q,cx,...
+             P,cxN] = optargin{:};
+            
+            x0 = sdpvar(nx,1,'full');
+            X = sdpvar(N,nx,'full');
+            Umpc = sdpvar(N,nu,'full');
+            u0mpc = Umpc(1,:)';
+            Gamma = sdpvar(N*nu,N*nu,'full');
+            delta = sdpvar(N*nu,1,'full');
+            %
+            xmin = this.model.xLim(1); xmax = this.model.xLim(2);
+            umin = this.model.uLim(1); umax = this.model.uLim(2);
+            
+            con = [];
+            if ~isempty(xmax)&&~isempty(xmin)
+                con = con + [xmin <= X , X <= xmax , umin <= Umpc , Umpc <= umax ];
+            end
+            if ~isempty(umax)&&~isempty(umin)
+                con = con + [X' == model.A*[x0 , X(1:end-1,:)'] + model.B*Umpc' ]; % dynamics
+            end
+            % COST
+            
+            Hx = blkdiag(kron(eye(N-1),Q),P);
+            hx = [kron(ones(N-1,1),cx);cxN];
+            Hu = kron(eye(N),R);
+            hu = kron(ones(N,1),cu);
+            cost = 0.5 * Umpc'*Hu*Umpc + hu'*Umpc + ...
+                   0.5 * X'*Hx*X + hx'*X + ...
+                   0.5 * Umpc'*Gamma*Umpc + delta'*Umpc ;
+            
+            %
+            ops = sdpsettings('verbose',1,'solver',solver);
+            
+            opt = optimizer(con,cost,ops,{x0,Gamma,delta},{u0mpc,Umpc});
+            
+        end
+                
+        
+        % These are the callbacks
+        function simple_callback(this, s)
+            fprintf('At %d %s\n', this.currentSimTime, s);
+        end
+        
+        function onInit(this)
+            % This function is called everytime the simulation starts, so
+            % use it to initialize the state and outputs of this node.
+            % Although it's not required, all outputs of this node should
+            % be initialized properly here.
+            
+            % Here we set the initial power input of the battery to some
+            % value
+            this.u = 0;
+            this.U = zeros(this.Nhorizon, 1);
+            this.output('u', this.u);
+            this.output('U', this.U);
+            
+            fprintf('At %d INIT\n', this.currentSimTime);
+        end
+        
+        function obtainstate(self)
+        % Get the state from plant and save it, then send U to aggregator
+            
+            %disp(['Obtain state at ', num2str(self.currentSimTime)]);
+            self.x0 = self.input('x');
+            % self.output('U', self.U);
+        end
+        
+        function admmiter(this, whichinput)
+        % This is the main computation function of this node.
+        % It performs the iteration of the distributed algorithm and is
+        % called each and every time the global node sends either Gamma or delta to the local
+        % node.
+        % whichinput = 1 for Gamma and 2 for delta.
+        % Once both have been received, it should compute U and send U to the aggregator.
+            switch whichinput
+              case 1
+                this.Gamma = this.input('Gamma');
+              case 2
+                this.delta = this.input('delta');
+              otherwise
+                warning('Wrong whichinput value in admm iteration callback.');
+                return;
+            end
+            if this.admm_events(whichinput)
+                % Already received the input --> should not receive it again
+                warning('In ADMM iteration callback, input %d has been received twice.', whichinput);
+            end
+            this.admm_events(whichinput) = true;
+            
+            if all(this.admm_events)
+                this.admm_events = [false, false]; % reset tracking
+                
+                % Perform the local iteration computation and send the result out
+                [solution,diagnostics] = this.mpc{{this.x0,this.Gamma,this.delta}};
+                this.u = solution{1};
+                this.U = solution{2};
+                
+                this.sendSync('U',this.U); % sendSync() must be used inside port event callbacks
+            end
+            % fprintf('At %d UPDATE_Y\n', this.currentSimTime);
+        end
+        
+        function computecontrol(self)
+            self.output('u', self.u);  % Send out the control value to the plant
+        end
+    end
+end
