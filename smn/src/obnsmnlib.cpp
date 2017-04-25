dff0724986b6ba407280e8f0334473bf77df7a3f
Truong Nghiem <truong.nghiem@gmail.com>
2017-04-25 16:37:53 +0200

    Skeleton code for SMN C library.

---
+++ smn/src/obnsmnlib.cpp
@@ -0,0 +1,17 @@
+/* -*- mode: C++; indent-tabs-mode: nil; -*- */
+/** \file
+ * \brief C dynamic library of the SMN library.
+ *
+ * The DLL is used by scripting languages such as Python, Julia, or by non-C++ languages such as C, to implement the SMN.
+ *
+ * This file is part of the openBuildNet simulation framework
+ * (OBN-Sim) developed at EPFL.
+ *
+ * \author Truong X. Nghiem (xuan.nghiem@epfl.ch)
+ */
+
+#include <memory>       // Smart pointers
+#include <obnsmnlib.h>
+
+using namespace std;
+
